c658223d83cda97dee59108e87618336b3b72829
zharnakov <al.zharnakov@yandex.ru>
2021-09-30 00:32:38 +0300

    наполнение класса Card

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,13 +1,46 @@
 class Card {
-    constructor (data, selector) {
-        cardElement.querySelector('.photo-grid__item-info-title').textContent = data.titleValue;
-        cardElement.querySelector('.photo-grid__item-image').src = data.linkValue;
-        cardElement.querySelector('.photo-grid__item-image').alt = titleValue;
+    constructor (data, cardSelector) {
+        this.cardSelector = cardSelector;
     }
 
-
 _getTemplate() {
+    const cardElement = document
+    .querySelector(this.cardSelector)
+    .content
+    .querySelector('.photo-grid__item')
+    .cloneNode(true);
 
+    return cardElement;
+}
+
+generateCard() {
+    this._element = this._getTemplate();
+    this._element.querySelector('.photo-grid__item-info-title').textContent = data.titleValue;
+    this._element.querySelector('.photo-grid__item-image').src = data.linkValue;
+    this._element.querySelector('.photo-grid__item-image').alt = data.titleValue;
+
+    return this._element
+}
+
+_removeCard() {
+    this._card =  this.generateCard();
+    this._card.remove()
+}
+
+// дополнительный попап при клике на элемент 
+_openPicturePopup() {
+    const picturePopup = document.getElementById('openPic');
+    picturePopup.src = data.linkValue;
+    openedPicture.alt = titleValue;
+
+    const crossButtonPicturePopup = document.getElementById('close_popup-pic');
+    const openedPicture = document.querySelector('.open-pic');
+    openedPictureLabel = document.querySelector('.open-pic-text');
+    
+}
+
+_buttonRemove() {
+    const buttonRemoveCard = cardElement.querySelector('.photo-grid__button');
 }
 
 }
